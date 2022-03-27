028cc2a5d5f1a2183c22e81785e60dd7aa218643
kivancaksoy <kvnc96@hotmail.com>
2022-03-27 12:14:13 +0300

    Revert "Swagger + All(No)ArgsConstructor"
    This reverts commit 018d327c78982e5fe8155d1750112739777107ab.

--- hrms/src/main/java/kvnc/hrms/entities/concretes/JobPosition.java
+++ hrms/src/main/java/kvnc/hrms/entities/concretes/JobPosition.java
@@ -3,28 +3,32 @@ package kvnc.hrms.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
 @Entity
 @Table(name="job_positions")
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class JobPosition {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue
 	@Column(name="id")
 	private int id;
 	
 	@Column(name="job_position")
 	private String jobPosition;
 	
+	public JobPosition(int id, String jobPosition) {
+		super();
+		this.id = id;
+		this.jobPosition = jobPosition;
+	}
+	
+	public JobPosition() {
+		
+	}
 
 }
